1a96ca13e00a22f4a9788035b19a2791de6e7ff6
maxhyeon <americano314@gmail.com>
2022-12-13 14:00:57 +0900

    add graceful shutdown

--- build-image-fat.sh
+++ build-image-fat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 ./mvnw clean package -DskipTests
-docker build ../ -f Dockerfile-fatjar -t boot-fatjar:1
+docker build . -f Dockerfile-fatjar -t boot-fatjar:1
 docker images boot-*
